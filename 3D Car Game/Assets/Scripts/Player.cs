66cdf047d3e2d30613c46c3796e0b3ecb6390d53
Hüseyin <103373602+HuseyinOztuna03@users.noreply.github.com>
2023-01-05 03:52:35 +0300

    rename tags

--- 3D Car Game/Assets/Scripts/Player.cs
+++ 3D Car Game/Assets/Scripts/Player.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public GameObject player;
+
     public float ms;
     public float moveInput;
+
+    public int hp;
+
+    private void Start()
+    {
+        hp = 2;
+    }
 
     void FixedUpdate()
     {
         moveInput = Input.GetAxis("Horizontal");
         transform.Translate(transform.right * Time.fixedDeltaTime * ms * moveInput);
         transform.Translate(transform.forward * Time.fixedDeltaTime * ms);
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("obstacle"))
+        {
+            hp--;
+            Destroy(other.gameObject);
+            if(hp < 1)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
